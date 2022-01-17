c98089d4f1b8a4edbcfb277141e92e913c6cb29a
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-01-16 19:48:27 -0800

    Working data fetch with dummy hook

--- scripts/module.js
+++ scripts/module.js
@@ -9,11 +9,17 @@ const moduleTag = 'Does it Hit?!';
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 class DoesItHit {
 	constructor() {
-		console.log(`${moduleTag} | Initializing. `);
-
-		Hooks.on('attackRoll', this._confirmHit);
+		Hooks.on('attackRoll', this._confirmHit.bind(this));
 	}
 
+	/**
+	 *
+	 * @param {*} item
+	 * @param {*} roll
+	 * @param {*} actor
+	 * @param {*} options
+	 * @returns
+	 */
 	_confirmHit(item, roll, actor, options) {
 		if (roll.options?.rollMode === 'selfRoll') return;
 
@@ -21,10 +27,19 @@ class DoesItHit {
 		const targets = [...(game.user.targets?.values() ?? [])].filter(
 			t => !!t.actor
 		);
-
 		if (!targets.length) return;
+
+		// Get Display Data
+		const displayData = targets.map(t => this._checkHit(t, roll));
+		console.log(displayData);
 	}
 
+	/**
+	 *
+	 * @param {*} token
+	 * @param {*} roll
+	 * @returns {}
+	 */
 	_checkHit(token, roll) {
 		// Get AC
 		const ac = token.actor.data.data.attributes.ac;
@@ -32,7 +47,17 @@ class DoesItHit {
 		const d20 = roll.dice[0];
 
 		// Check if critical hit or miss
-		const isCritHit = d20.faces === 20;
+		const isCritHit =
+			d20.faces === 20 &&
+			d20.values.length === 1 &&
+			d20.total >= (d20.options.critical ?? 20);
+
+		const isFumble =
+			d20.faces === 20 && d20.values.length === 1 && d20.total === 1;
+
+		const isHit = !isFumble && (isCritHit || rollTotal >= ac);
+
+		return { ac, isCritHit, isFumble, isHit, token };
 	}
 }
 
@@ -57,4 +82,26 @@ class DoesItHit {
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 Hooks.once('init', async function () {});
 
-Hooks.once('ready', async function () {});
+Hooks.once('ready', async function () {
+	console.log(`${moduleTag} | Initializing. `);
+
+	// Dummy hook
+	libWrapper.register(
+		moduleName,
+		'CONFIG.Actor.documentClass.prototype.constructItemCard',
+		function (wrapped, data) {
+			console.log(data);
+			const actor = this.data;
+			const item = actor.items.get(data.id);
+			if (data.attack?.roll) {
+				const roll = data.attack.roll;
+				Hooks.call('attackRoll', item, roll, actor, {});
+			}
+
+			return wrapped(data);
+		},
+		'WRAPPER'
+	);
+
+	new DoesItHit();
+});
