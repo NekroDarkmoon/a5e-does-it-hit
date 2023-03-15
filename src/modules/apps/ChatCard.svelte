6a5ab1f2b076c9ee5445f0daaea5a45335525ea4
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-15 16:20:29 -0700

    Adds HitCheckCard

--- src/modules/apps/ChatCard.svelte
+++ src/modules/apps/ChatCard.svelte
@@ -1,10 +1,20 @@
 <script>
 	import { TJSDocument } from '@typhonjs-fvtt/runtime/svelte/store';
+
+	import { moduleName, moduleTag } from '../constants';
+
+	import HitCheckCard from './HitCheckCard.svelte';
+
 	export let messageDocument;
 
-	const message = new TJSDocument(messageDocument);
+	function getBodyComponent() {
+		switch ($message?.flags[moduleName]?.cardType) {
+			case 'hitCheck':
+				return HitCheckCard;
+		}
+	}
 
-	console.log(message);
+	const message = new TJSDocument(messageDocument);
 </script>
 
-Hello
+<svelte:component this={getBodyComponent()} {message} />
