414aedcd7412b60fef7dbf439bdb1ee1c697f6fa
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-15 05:58:31 -0700

    Removes styles attribute from module.json

--- src/modules/HItCheck.js
+++ src/modules/HItCheck.js
@@ -1,7 +1,15 @@
 import { moduleName, modTag } from "./constants"; 
 
 export default class HitCheck {
-    constructor( ) {
+  constructor() {
+    Hooks.on('itemActivate', this.#handler.bind(this));
+  }
 
-    }
+  #handler(item, data) {
+    if (!data.action) return;
+  	const rolls = Object.values(data.action.rolls)
+			.filter((roll) => ['attack', 'damage', 'healing'].includes(roll.type));
+
+		console.log(rolls);
+  }
 }
