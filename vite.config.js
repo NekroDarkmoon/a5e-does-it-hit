771d290bd71456f766ce1eec4b95f987d91364fa
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-15 05:18:35 -0700

    Updates dependencies and changes module name from does-it-hit to a5e-does-it-hit

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,50 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import preprocess from 'svelte-preprocess';
-
-const path = require('path');
+import path from 'path';
 
 const config = {
-	root: 'src/',
-	base: '/modules/does-it-hit-a5e/',
-	publicDir: path.resolve(__dirname, 'public'),
-	server: {
-		port: 30001,
-		open: '/game',
-		proxy: {
-			'^(?!/modules/does-it-hit-a5e/)': 'http://localhost:30000',
-			'/socket.io': {
-				target: 'ws://localhost:30000',
-				ws: true,
-			},
-		},
-	},
-	build: {
-		outDir: path.resolve(__dirname, 'dist'),
-		emptyOutDir: true,
-		sourcemap: true,
-		brotliSize: true,
-		lib: {
-			name: 'Does it Hit?! (A5E)',
-			entry: path.resolve(__dirname, 'src/index.js'),
-			formats: ['es'],
-			fileName: () => 'index.js',
-		},
-	},
-	plugins: [
-		svelte({
-			preprocess: preprocess(),
-			onwarn: (warning, handler) => {
-				// Suppress `a11y-missing-attribute` for missing href in <a> links.
-				// Foundry doesn't follow accessibility rules.
-				if (
-					warning.message.includes('<a> element should have an href attribute')
-				) {
-					return;
-				}
+  root: 'src/',
+  base: '/modules/does-it-hit-a5e/',
+  publicDir: path.resolve(__dirname, 'public'),
+  server: {
+    port: 30001,
+    open: true,
+    proxy: {
+      '^(?!/modules/does-it-hit-a5e/)': 'http://localhost:30000',
+      '/socket.io': {
+        target: 'ws://localhost:30000',
+        ws: true
+      }
+    }
+  },
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    sourcemap: true,
+    brotliSize: true,
+    lib: {
+      name: 'Does it Hit?! (A5E)',
+      entry: path.resolve(__dirname, 'src/index.js'),
+      formats: ['es'],
+      fileName: () => 'index.js'
+    }
+  },
+  plugins: [
+    svelte({
+      preprocess: preprocess(),
+      onwarn: (warning, handler) => {
+        // Suppress `a11y-missing-attribute` for missing href in <a> links.
+        // Foundry doesn't follow accessibility rules.
+        if (warning.message.includes('<a> element should have an href attribute')) return;
+        if (warning.code === 'a11y-click-events-have-key-events') return;
 
-				// Let Rollup handle all other warnings normally.
-				handler(warning);
-			},
-		}),
-	],
+        console.log(warning);
+
+        // Let Rollup handle all other warnings normally.
+        handler(warning);
+      }
+    })
+  ]
 };
 
 export default config;
