9be9c95aab922e115987ff8e19cca91ba7168d14
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-17 04:10:17 -0700

    WIP - Work on HitSection

---
+++ src/apps/components/HitSection.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { localize } from '@typhonjs-fvtt/runtime/svelte/helper';
+
+	export let target;
+
+	console.log($target);
+</script>
+
+<div class="hit-section">
+	<img src={$target.texture.src} alt="" width="50rem" height="50rem" />
+	<span class="hit-section__name">
+		{$target.displayName ?? $target.name}
+	</span>
+</div>
