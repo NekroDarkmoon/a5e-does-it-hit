12fd16cc40d6c1eda4ad968bb3387c132e2493cd
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-17 05:56:43 -0700

    WIP - Basic styling for damagesection

--- src/apps/components/DamageSection.svelte
+++ src/apps/components/DamageSection.svelte
@@ -27,8 +27,20 @@
 				<option value="${damage * 0.5}">1/2</option>
 				<option value="${damage * 0.25}">1/4</option>
 			</select>
+
+			<button class="apply-button">
+				<i class="fas fa-check" />
+			</button>
+
+			<button class="reset-button">
+				<i class="fas fa-undo" />
+			</button>
 		</div>
 	{/each}
+
+	<hr class="a5e-rule u-pt-md" />
+
+	<!-- Total -->
 </div>
 
 <style lang="scss">
@@ -40,16 +52,32 @@
 	.damage__container {
 		display: flex;
 		gap: 0.25rem;
-		justify-content: center;
 		align-items: center;
 	}
 
 	.damage-data {
-		width: 60%;
+		flex-grow: 1;
 	}
 
 	.multiplier {
 		font-size: 0.694rem;
-		height: 1.25rem;
+		height: 1rem;
+	}
+
+	button {
+		// display: flex;
+		position: relative;
+		height: 1rem;
+		width: 1rem;
+		align-items: center;
+		justify-content: center;
+
+		i {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			font-size: 0.694rem;
+		}
 	}
 </style>
