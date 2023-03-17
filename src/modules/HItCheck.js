4135d10b552419171a07793a69887cebef6dc505
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-17 01:55:40 -0700

    Adds preparation handler for attackRoll

--- src/modules/HItCheck.js
+++ src/modules/HItCheck.js
@@ -1,69 +1,86 @@
-import { moduleName, moduleTag } from "./constants"; 
+import { moduleName, moduleTag } from './constants';
 
 export default class HitCheck {
-  constructor() {
-    Hooks.on('a5e.itemActivate', this.#handler.bind(this));
-  }
+	constructor() {
+		Hooks.on('a5e.itemActivate', this.#handler.bind(this));
+	}
 
-  #handler(item, data) {
-    if (!data.action) return;
-  	const rolls = Object.values(data.rolls)
-			.filter((roll) => ['attack', 'damage', 'healing'].includes(roll.type));
+	#handler(item, data) {
+		if (!data.action) return;
+		const rolls = Object.values(data.rolls).filter(roll =>
+			['attack', 'damage', 'healing'].includes(roll.type)
+		);
 		if (!rolls.length) return;
 
-    // Get Targets
-    const targets = [...game.user.targets?.values() ?? []].filter(
-      t => !!t.actor
-    );
-    if (!targets.length) return;
+		// Get Targets
+		const targets = [...(game.user.targets?.values() ?? [])].filter(
+			t => !!t.actor
+		);
+		if (!targets.length) return;
 
-    // Filter rolls into attack, damage, and healing rolls.
-    const attackData = this.#prepareAttackRollData(rolls.filter((roll) => roll.type === 'attack')[0]);
-    const damageRolls = rolls.filter((roll) => roll.type === 'damage');
-    const healingRolls = rolls.filter((roll) => roll.type === 'healing');
+		// Filter rolls into attack, damage, and healing rolls.
+		const attackData = this.#prepareAttackRollData(
+			rolls.filter(roll => roll.type === 'attack')[0]
+		);
+		const damageRolls = this.#prepareDamageData(
+			rolls.filter(roll => roll.type === 'damage')
+		);
+		const healingRolls = this.#prepareHealingData(
+			rolls.filter(roll => roll.type === 'healing')
+		);
 
+		console.log(data);
 
-    console.log(data);
+		const chatData = {
+			user: game.user?.id,
+			speaker: ChatMessage.getSpeaker({ actor: item.parent }),
+			type: CONST.CHAT_MESSAGE_TYPES.OTHER,
+			sound: CONFIG.sounds.dice,
+			flags: {
+				[moduleName]: {
+					actorId: item.parent.uuid,
+					cardType: 'hitCheck',
+					rollData: data.rolls.map(({ roll, ...rollData }) => rollData),
+				},
+			},
+			content: '<article></article>',
+		};
 
+		ChatMessage.create(chatData);
+	}
 
-    const chatData = {
-      user: game.user?.id,
-      speaker: ChatMessage.getSpeaker({ actor: item.parent }),
-      type: CONST.CHAT_MESSAGE_TYPES.OTHER,
-      sound: CONFIG.sounds.dice,
-      flags: {
-        [moduleName]: {
-          actorId: item.parent.uuid,
-          cardType: 'hitCheck',
-          rollData: data.rolls.map(({roll, ...rollData}) => rollData)
-          
-        }
-      },
-      content: '<article></article>'
-    }
+	/**
+	 *
+	 * @param {Object} attackRoll
+	 */
+	#prepareAttackRollData(attackRoll) {
+		if (!attackRoll) return {};
 
-    ChatMessage.create(chatData);
-  }
+		const d20 = attackRoll?.roll?.dice?.[0];
+		const isFumble = d20?.faces === 20 && d20?.total === 1;
 
-  /**
-   * 
-   * @param {Object} attackRoll 
-   */
-  #prepareAttackRollData(attackRoll) {
-    
+		const data = {
+			isCrit: attackRoll?.isCrit,
+			isFumble,
+			rollTotal: attackRoll?.roll?.total,
+		};
 
-  }
+		return data;
+	}
 
-  /**
-   * 
-   * @param {Array<Object>} damageRolls 
-   */
-  #prepareDamageData(damageRolls) {}
+	/**
+	 *
+	 * @param {Array<Object>} damageRolls
+	 */
+	#prepareDamageData(damageRolls) {
+		if (!damageRolls.length) return;
+	}
 
-
-  /**
-   * 
-   * @param {Array<Object>} healingRolls 
-   */
-  #prepareHealingData(healingRolls) {}
+	/**
+	 *
+	 * @param {Array<Object>} healingRolls
+	 */
+	#prepareHealingData(healingRolls) {
+		if (!healingRolls.length) return;
+	}
 }
