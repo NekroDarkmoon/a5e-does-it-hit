d6a160fda8dd2f7eae8e892314df3a7fd9312dd2
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-17 06:46:00 -0700

    Adds dynmaic damage update

--- src/apps/components/DamageSection.svelte
+++ src/apps/components/DamageSection.svelte
@@ -5,27 +5,33 @@
 	export let damageData;
 	export let cardData;
 
-	console.log(cardData);
-
 	let targetFlag = cardData.targetData?.[$target.uuid];
+	let damageOption = damageData.map(({ damage }) => damage);
 
+	function updateDamageOptions(event) {
+		const value = Number(event.value);
+		damageOption = damageData.map(({ damage }) => damage * value);
+	}
+
+	$: totalDamage = damageOption.reduce((a, b) => a + b, 0);
 	$: hp = targetFlag?.hp ?? $target.actor.system.attributes.hp.value;
 </script>
 
 <div class="damage-section">
-	{#each damageData as { canCrit, damage, damageType }}
+	{#each damageData as { canCrit, damage, damageType }, idx}
 		<div class="damage__container">
 			<span class="damage-data">
 				<!--  -->
-				{damage}
+				{damageOption[idx]}
 				{damageType}
 			</span>
 
-			<select class="multiplier">
-				<option value="${damage}">Base</option>
-				<option value="${damage * 2}">2</option>
-				<option value="${damage * 0.5}">1/2</option>
-				<option value="${damage * 0.25}">1/4</option>
+			<select class="multiplier" bind:value={damageOption[idx]}>
+				<option value={damage * 0}>None</option>
+				<option value={damage}>Base</option>
+				<option value={damage * 2}>2</option>
+				<option value={damage * 0.5}>1/2</option>
+				<option value={damage * 0.25}>1/4</option>
 			</select>
 
 			<button class="apply-button">
@@ -40,7 +46,30 @@
 
 	<hr class="a5e-rule u-pt-md" />
 
-	<!-- Total -->
+	<div class="damage__container">
+		<span class="damage-data">
+			{totalDamage}
+		</span>
+
+		<select
+			class="multiplier"
+			on:change={({ target }) => updateDamageOptions(target)}
+		>
+			<option value={0}>None</option>
+			<option value={1} selected>Base</option>
+			<option value={2}>2</option>
+			<option value={0.5}>1/2</option>
+			<option value={0.25}>1/4</option>
+		</select>
+
+		<button class="apply-button">
+			<i class="fas fa-check" />
+		</button>
+
+		<button class="reset-button">
+			<i class="fas fa-undo" />
+		</button>
+	</div>
 </div>
 
 <style lang="scss">
@@ -53,8 +82,17 @@
 		display: flex;
 		gap: 0.25rem;
 		align-items: center;
-	}
+		padding-inline: 0.25rem;
+		padding-block: 0.125rem;
 
+		&:not(:last-child) {
+			// background-color: dodgerblue;
+		}
+
+		&:nth-child(odd):not(:last-child) {
+			background-color: #ccc;
+		}
+	}
 	.damage-data {
 		flex-grow: 1;
 	}
@@ -71,6 +109,8 @@
 		width: 1rem;
 		align-items: center;
 		justify-content: center;
+		background: transparent;
+		border: none;
 
 		i {
 			position: absolute;
