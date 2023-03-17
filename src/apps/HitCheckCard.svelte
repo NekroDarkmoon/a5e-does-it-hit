de2ff6fa41a76b9548d451398ab17fe5a107fb9a
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-17 04:10:00 -0700

    Adds reducer for targets

--- src/apps/HitCheckCard.svelte
+++ src/apps/HitCheckCard.svelte
@@ -1,31 +1,31 @@
 <script>
 	import { localize } from '@typhonjs-fvtt/runtime/svelte/helper';
 	import { TJSDocument } from '@typhonjs-fvtt/runtime/svelte/store';
+	import { DynMapReducer } from '@typhonjs-fvtt/runtime/svelte/store';
 
 	import { moduleName, moduleTag } from '../modules/constants';
 	import HitCheck from '../modules/HitCheck';
+
+	import DamageSection from './components/DamageSection.svelte';
+	import HitSection from './components/HitSection.svelte';
 
 	export let message;
 
 	let cardData = $message.flags?.[moduleName];
 
 	const attacker = new TJSDocument(fromUuidSync(cardData.actorId));
-	const targets = cardData.targets.map(t => new TJSDocument(fromUuidSync(t)));
+
+	const targets = new Map(
+		cardData.targets.map(t => [t, new TJSDocument(fromUuidSync(t))])
+	);
+
+	const reducer = new DynMapReducer(targets);
+	console.log(reducer.data);
 </script>
 
 <section class="section-container">
-	{#each targets as target}
-		<div class="hit-section">
-			<!--  -->
-			<img src="" alt="" />
-			<span class="hit-section__name">
-				{target.name}
-			</span>
-		</div>
-
-		<div class="damage-section">
-			<!--  -->
-		</div>
+	{#each [...$reducer] as target}
+		<HitSection {target} />
 	{/each}
 </section>
 
