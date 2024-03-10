87e33105d2bfb348716bc21c971d52275c111e2c
Phil <me@phillip.best>
2024-03-10 12:24:01 +0000

    Updates the hit section classes to follow a more consistent naming convention. Adds a new hit, crit, or miss label to the hit roll section. Replaces the die and shield SVG backgrounds with Font Awesome icons.

--- src/apps/components/HitSection.svelte
+++ src/apps/components/HitSection.svelte
@@ -9,61 +9,28 @@
 	$: ac = targetFlag?.ac ?? $target?.actor.system.attributes.ac.value ?? 0;
 </script>
 
-<div class="hit-section">
+<div class="dih-hit-section">
 	{#if $target}
 		<img
-			class="hit-section__img"
+			class="dih-target-img"
 			src={$target.texture.src}
 			alt={$target.displayName ?? $target.name}
 		/>
 
-		<span class="hit-section__name">
+		<span class="dih-target-name">
 			{$target.name}
 		</span>
 
 		{#if attackData.rollTotal}
 			<div
-				class="hit-section__roll"
-				class:roll-background--crit={attackData.isCrit}
-				class:roll-background--fumble={attackData.isFumble}
+				class="dih-hit-section__roll"
+				class:dih-hit-section__roll--hit={hit()}
+				class:dih-hit-section__roll--crit={attackData.isCrit}
 			>
-				<svg
-					class="roll-background"
-					class:roll-background--hit={hit()}
-					x="0px"
-					y="0px"
-					viewBox="-16 0 512 512"
-					xml:space="preserve"
-				>
-					<path
-						d="M106.75 215.06L1.2 370.95c-3.08 5 .1 11.5 5.93 12.14l208.26 22.07-108.64-190.1zM7.41 315.43L82.7 193.08 6.06
-                147.1c-2.67-1.6-6.06.32-6.06 3.43v162.81c0 4.03 5.29 5.53 7.41 2.09zM18.25 423.6l194.4 87.66c5.3 2.45 11.35-1.43
-                11.35-7.26v-65.67l-203.55-22.3c-4.45-.5-6.23 5.59-2.2 7.57zm81.22-257.78L179.4 22.88c4.34-7.06-3.59-15.25-10.78-11.14L17.81
-                110.35c-2.47 1.62-2.39 5.26.13 6.78l81.53 48.69zM240 176h109.21L253.63 7.62C250.5 2.54 245.25 0 240 0s-10.5 2.54-13.63 7.62L130.79
-                176H240zm233.94-28.9l-76.64 45.99 75.29 122.35c2.11 3.44 7.41 1.94 7.41-2.1V150.53c0-3.11-3.39-5.03-6.06-3.43zm-93.41
-                18.72l81.53-48.7c2.53-1.52 2.6-5.16.13-6.78l-150.81-98.6c-7.19-4.11-15.12 4.08-10.78 11.14l79.93 142.94zm79.02 250.21L256 
-                438.32v65.67c0 5.84 6.05 9.71 11.35 7.26l194.4-87.66c4.03-1.97 2.25-8.06-2.2-7.56zm-86.3-200.97l-108.63 190.1 
-                208.26-22.07c5.83-.65 9.01-7.14 5.93-12.14L373.25 215.06zM240 208H139.57L240 383.75 340.43 208H240z"
-					/>
-				</svg>
-
 				{attackData.rollTotal ?? ''}
 			</div>
 
-			<div class="hit-section__ac">
-				<svg
-					class="ac-background"
-					version="1.1"
-					x="0px"
-					y="0px"
-					viewBox="0 0 90 100"
-					xml:space="preserve"
-				>
-					<path
-						d="M45,100C-2.6,79.3,0,12.6,0,12.6c0-2.2,1.8-4,4.4-4.6l39.1-7.9C44,0,44.5,0,45,0c0.5,0,1,0,1.4,0.1L85.5,8
-            c2.6,0.5,4.4,2.4,4.4,4.6C90,12.6,92.6,79.3,45,100L45,100z"
-					/>
-				</svg>
+			<div class="dih-hit-section__ac">
 				{ac}
 			</div>
 		{/if}
@@ -71,89 +38,89 @@
 </div>
 
 <style lang="scss">
-	.hit-section {
+	.dih-hit-section {
 		display: grid;
 		grid-template-areas: 'img name hitRoll ac';
-		grid-template-columns: 3rem 1fr 4ch 4ch;
-		gap: 0.25rem;
+		grid-template-columns: 2.5rem 1fr 4ch 4ch;
+		gap: 0.5rem;
 		align-items: center;
+		font-size: 1rem;
+
+		&__roll {
+			--die-background-color: rgba(168, 3, 0, 0.2);
+			--accent-text-color: rgb(168, 3, 0);
+
+			grid-area: hitRoll;
+			position: relative;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100%;
+			font-weight: bold;
+
+			&::before {
+				content: '\f6cf';
+				position: absolute;
+				font-family: 'Font Awesome 6 Pro';
+				color: var(--die-background-color);
+				font-size: 2rem;
+			}
+
+			&::after {
+				content: 'MISS';
+				position: absolute;
+				bottom: 0;
+				font-size: 0.694rem;
+				color: var(--accent-text-color);
+			}
+
+			&--hit {
+				--die-background-color: rgba(24, 82, 11, 0.2);
+				--accent-text-color: rgba(24, 82, 11);
+
+				&::after {
+					content: 'HIT';
+				}
+			}
+
+			&--crit::after {
+				content: 'CRIT';
+			}
+		}
+
+		&__ac {
+			grid-area: ac;
+			position: relative;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100%;
+			font-weight: bold;
+
+			&::before {
+				content: '\f132';
+				position: absolute;
+				font-family: 'Font Awesome 6 Pro';
+				color: rgba(0, 0, 0, 0.15);
+				font-size: 2rem;
+			}
+		}
 	}
 
-	.hit-section__name {
+	.dih-target-name {
 		grid-area: name;
 		margin: 0;
+		font-size: 1rem;
+		font-weight: 700;
 		text-overflow: ellipsis;
 		white-space: nowrap;
 		overflow: hidden;
-		font-size: 0.833rem;
 	}
 
-	.hit-section__img {
+	.dih-target-img {
 		grid-area: img;
 		border: none;
 		width: 2.5rem;
 		height: 2.5rem;
-	}
-
-	.hit-section__roll {
-		grid-area: hitRoll;
-		position: relative;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100%;
-		font-weight: bold;
-	}
-
-	.hit-section__ac {
-		grid-area: ac;
-		position: relative;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100%;
-		font-weight: bold;
-	}
-
-	.roll-background {
-		position: absolute;
-		height: 1.8rem;
-		bottom: 0.35rem;
-		fill: rgba(170 2 0 / 0.2);
-		z-index: 0;
-
-		&--hit {
-			fill: rgba(24 82 11 / 0.2);
-		}
-
-		&--fumble:after {
-			content: 'fumble';
-			position: absolute;
-			bottom: -2px;
-			font-family: 'Modesto Condensed';
-			font-size: 0.694rem;
-			text-transform: lowercase;
-			background: none;
-			color: rgb(170 2 0);
-		}
-
-		&--crit:after {
-			content: 'crit';
-			position: absolute;
-			bottom: -2px;
-			font-family: 'Modesto Condensed';
-			font-size: 0.694rem;
-			text-transform: lowercase;
-			background: none;
-			color: rgb(24 82 11);
-		}
-	}
-
-	.ac-background {
-		position: absolute;
-		height: 2rem;
-		bottom: 0.2rem;
-		fill: rgba(0, 0, 0, 0.15);
-		z-index: 0;
 	}
 </style>
